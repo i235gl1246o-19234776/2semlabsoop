e9b0367d54adfd84b20b80298772631adf57b5f5
Никита Жуков <zkovdn@gmail.com>
2025-09-12 19:17:11 +0400

    Реализация AndThen

--- 2LabOOP/src/main/java/functions/MathFunction.java
+++ 2LabOOP/src/main/java/functions/MathFunction.java
@@ -4,4 +4,10 @@ public interface MathFunction {
 
     double apply(double x);
 
+
+
+    default MathFunction andThen(MathFunction afterFunction) {
+        return new CompositeFunction(this, afterFunction);
+    }
+
 }
