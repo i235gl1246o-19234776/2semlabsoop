1267cd50ef2585526960bf3f01756c01bd0bbe51
Никита Жуков <zkovdn@gmail.com>
2025-09-20 23:22:02 +0400

    output корректен

--- 3LabOOP/src/main/java/io/FunctionsIO.java
+++ 3LabOOP/src/main/java/io/FunctionsIO.java
@@ -1,7 +1,29 @@
 package io;
 
+import functions.ArrayTabulatedFunction;
+import functions.LinkedListTabulatedFunction;
+import functions.TabulatedFunction;
+import functions.Point;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException("Не удается создать экземпляр служебного класса");
     }
+
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) throws IOException {
+        PrintWriter printWriter = new PrintWriter(writer);
+
+        printWriter.println(function.getCount());
+
+        for (Point point : function) {
+            printWriter.printf("%f %f\n", point.x, point.y);
+        }
+
+        printWriter.flush();
+    }
+
 }
