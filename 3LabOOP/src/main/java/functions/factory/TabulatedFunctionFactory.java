394f7d3596a79510f4024bad979b095f8076b892
Владислав Николаев <eshteben@email.com>
2025-09-20 13:19:44 +0400

    Добавлено задание x* где надо добавить метод в абстрактную фабрику

--- 3LabOOP/src/main/java/functions/factory/TabulatedFunctionFactory.java
+++ 3LabOOP/src/main/java/functions/factory/TabulatedFunctionFactory.java
@@ -1,8 +1,14 @@
 package functions.factory;
 
+import functions.StrictTabulatedFunction;
 import functions.TabulatedFunction;
 
 public interface TabulatedFunctionFactory {
     TabulatedFunction create(double[] xValues, double[] yValues);
+
+    default TabulatedFunction createStrict(double[] xValues, double[] yValues){
+        TabulatedFunction baseFunction = create(xValues, yValues);
+        return new StrictTabulatedFunction(baseFunction);
+    }
 
 }
--- 3LabOOP/src/test/java/functions/factory/TabulatedFunctionFactoryTest.java
+++ 3LabOOP/src/test/java/functions/factory/TabulatedFunctionFactoryTest.java
@@ -3,6 +3,7 @@ package functions.factory;
 import functions.TabulatedFunction;
 import functions.ArrayTabulatedFunction;
 import functions.LinkedListTabulatedFunction;
+import functions.StrictTabulatedFunction;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
@@ -88,6 +89,135 @@ class TabulatedFunctionFactoryTest {
 
         assertEquals(1.0, function.getX(0), 0.0001);
         assertEquals(10.0, function.getY(0), 0.0001);
+    }
+
+
+    @Test
+    @DisplayName("ArrayTabulatedFunctionFactory.createStrict должен создавать StrictTabulatedFunction")
+    void testArrayFactoryCreateStrict() {
+        TabulatedFunctionFactory factory = new ArrayTabulatedFunctionFactory();
+        double[] xValues = {1.0, 2.0, 3.0};
+        double[] yValues = {10.0, 20.0, 30.0};
+
+        TabulatedFunction function = factory.createStrict(xValues, yValues);
+
+        assertNotNull(function);
+        assertTrue(function instanceof StrictTabulatedFunction);
+        assertEquals(3, function.getCount());
+        assertEquals(1.0, function.getX(0), 0.0001);
+        assertEquals(10.0, function.getY(0), 0.0001);
+    }
+
+    @Test
+    @DisplayName("LinkedListTabulatedFunctionFactory.createStrict должен создавать StrictTabulatedFunction")
+    void testLinkedListFactoryCreateStrict() {
+        TabulatedFunctionFactory factory = new LinkedListTabulatedFunctionFactory();
+        double[] xValues = {1.0, 2.0, 3.0};
+        double[] yValues = {10.0, 20.0, 30.0};
+
+        TabulatedFunction function = factory.createStrict(xValues, yValues);
+
+        assertNotNull(function);
+        assertTrue(function instanceof StrictTabulatedFunction);
+        assertEquals(3, function.getCount());
+        assertEquals(1.0, function.getX(0), 0.0001);
+        assertEquals(10.0, function.getY(0), 0.0001);
+    }
+
+    @Test
+    @DisplayName("createStrict должен запрещать интерполяцию для ArrayFactory")
+    void testArrayFactoryCreateStrictNoInterpolation() {
+        TabulatedFunctionFactory factory = new ArrayTabulatedFunctionFactory();
+        double[] xValues = {1.0, 2.0, 3.0};
+        double[] yValues = {10.0, 20.0, 30.0};
+
+        TabulatedFunction function = factory.createStrict(xValues, yValues);
+
+        assertEquals(10.0, function.apply(1.0), 0.0001);
+        assertEquals(20.0, function.apply(2.0), 0.0001);
+
+        assertThrows(UnsupportedOperationException.class, () -> function.apply(1.5));
+        assertThrows(UnsupportedOperationException.class, () -> function.apply(0.5));
+        assertThrows(UnsupportedOperationException.class, () -> function.apply(3.5));
+    }
+
+    @Test
+    @DisplayName("createStrict должен запрещать интерполяцию для LinkedListFactory")
+    void testLinkedListFactoryCreateStrictNoInterpolation() {
+        TabulatedFunctionFactory factory = new LinkedListTabulatedFunctionFactory();
+        double[] xValues = {1.0, 2.0, 3.0};
+        double[] yValues = {10.0, 20.0, 30.0};
+
+        TabulatedFunction function = factory.createStrict(xValues, yValues);
+
+        assertEquals(10.0, function.apply(1.0), 0.0001);
+        assertEquals(20.0, function.apply(2.0), 0.0001);
+
+        assertThrows(UnsupportedOperationException.class, () -> function.apply(1.5));
+        assertThrows(UnsupportedOperationException.class, () -> function.apply(0.5));
+        assertThrows(UnsupportedOperationException.class, () -> function.apply(3.5));
+    }
+
+    @Test
+    @DisplayName("createStrict должен сохранять независимость от исходных массивов")
+    void testCreateStrictIndependenceFromArrays() {
+        TabulatedFunctionFactory factory = new ArrayTabulatedFunctionFactory();
+        double[] xValues = {1.0, 2.0, 3.0};
+        double[] yValues = {10.0, 20.0, 30.0};
+
+        TabulatedFunction function = factory.createStrict(xValues, yValues);
+
+        xValues[0] = 100.0;
+        yValues[0] = 200.0;
+
+        assertEquals(1.0, function.getX(0), 0.0001);
+        assertEquals(10.0, function.getY(0), 0.0001);
+    }
+
+    @Test
+    @DisplayName("createStrict с большими массивами для ArrayFactory")
+    void testArrayFactoryCreateStrictWithManyElements() {
+        TabulatedFunctionFactory factory = new ArrayTabulatedFunctionFactory();
+        double[] xValues = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0};
+        double[] yValues = {1.0, 4.0, 9.0, 16.0, 25.0, 36.0, 49.0, 64.0, 81.0, 100.0};
+
+        TabulatedFunction function = factory.createStrict(xValues, yValues);
+
+        assertNotNull(function);
+        assertTrue(function instanceof StrictTabulatedFunction);
+        assertEquals(10, function.getCount());
+        assertEquals(10.0, function.getX(9), 0.0001);
+        assertEquals(100.0, function.getY(9), 0.0001);
+    }
+
+    @Test
+    @DisplayName("createStrict с большими массивами для LinkedListFactory")
+    void testLinkedListFactoryCreateStrictWithManyElements() {
+        TabulatedFunctionFactory factory = new LinkedListTabulatedFunctionFactory();
+        double[] xValues = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0};
+        double[] yValues = {1.0, 4.0, 9.0, 16.0, 25.0, 36.0, 49.0, 64.0, 81.0, 100.0};
+
+        TabulatedFunction function = factory.createStrict(xValues, yValues);
+
+        assertNotNull(function);
+        assertTrue(function instanceof StrictTabulatedFunction);
+        assertEquals(10, function.getCount());
+        assertEquals(10.0, function.getX(9), 0.0001);
+        assertEquals(100.0, function.getY(9), 0.0001);
+    }
+
+    @Test
+    @DisplayName("createStrict должен разрешать модификацию Y значений")
+    void testCreateStrictAllowsSetY() {
+        TabulatedFunctionFactory factory = new ArrayTabulatedFunctionFactory();
+        double[] xValues = {1.0, 2.0, 3.0};
+        double[] yValues = {10.0, 20.0, 30.0};
+
+        TabulatedFunction function = factory.createStrict(xValues, yValues);
+
+        function.setY(1, 25.0);
+        assertEquals(25.0, function.getY(1), 0.0001);
+        assertEquals(25.0, function.apply(2.0), 0.0001);
     }
 
 }
