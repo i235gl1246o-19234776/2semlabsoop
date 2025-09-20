8c15b57582300532462a46d68b54dfcb80f4a962
Никита Жуков <zkovdn@gmail.com>
2025-09-20 13:26:47 +0400

    Производные

--- 3LabOOP/src/main/java/operations/RightSteppingDifferentialOperator.java
+++ 3LabOOP/src/main/java/operations/RightSteppingDifferentialOperator.java
@@ -1,4 +1,22 @@
 package operations;
 
-public class RightSteppingDifferentialOperator {
+import functions.MathFunction;
+
+public class RightSteppingDifferentialOperator extends SteppingDifferentialOperator<MathFunction>{
+
+    public RightSteppingDifferentialOperator(double step) {
+        super(step);
+    }
+    @Override
+    public MathFunction derive(MathFunction f) {
+        if (f == null) {
+            throw new IllegalArgumentException("Функция не пуста");
+        }
+        return new MathFunction() {
+            @Override
+            public double apply(double x) {
+                return (f.apply(x + step)- f.apply(x)) / step;
+            }
+        };
+    }
 }
