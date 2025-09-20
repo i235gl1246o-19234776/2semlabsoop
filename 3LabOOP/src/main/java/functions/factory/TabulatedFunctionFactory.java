b226bad2d95e6edec7dbf38963ac930ca2f6d335
Владислав Николаев <eshteben@email.com>
2025-09-20 13:37:13 +0400

    Добавлен метод с оберткой двух декораторов

--- 3LabOOP/src/main/java/functions/factory/TabulatedFunctionFactory.java
+++ 3LabOOP/src/main/java/functions/factory/TabulatedFunctionFactory.java
@@ -2,6 +2,7 @@ package functions.factory;
 
 import functions.StrictTabulatedFunction;
 import functions.TabulatedFunction;
+import functions.UnmodifiableTabulatedFunction;
 
 public interface TabulatedFunctionFactory {
     TabulatedFunction create(double[] xValues, double[] yValues);
@@ -9,6 +10,12 @@ public interface TabulatedFunctionFactory {
     default TabulatedFunction createStrict(double[] xValues, double[] yValues){
         TabulatedFunction baseFunction = create(xValues, yValues);
         return new StrictTabulatedFunction(baseFunction);
+    }
+
+    default TabulatedFunction createStrictUnmodifiable(double[] xValues, double[] yValues){
+        TabulatedFunction baseFunction = create(xValues, yValues);
+        TabulatedFunction strictFunction = new StrictTabulatedFunction(baseFunction);
+        return new UnmodifiableTabulatedFunction(strictFunction);
     }
 
 }
